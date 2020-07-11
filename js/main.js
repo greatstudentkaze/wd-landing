1005238daefac92cda4c2f7386b4a307671ccf39
Ivan Nefagin <dfyz0r@list.ru>
2020-07-11 23:51:51 +0300

    сделано меню для расширений меньше 992px

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
-const video = document.querySelector('.workstyle__media-video');
-
-const videoWrapper = document.querySelector('.workstyle__media'),
-    videoTempWrapper = document.querySelector('.workstyle__media-after');
+const video = document.querySelector('.workstyle__media-video'),
+    videoWrapper = document.querySelector('.workstyle__media'),
+    videoTempWrapper = document.querySelector('.workstyle__media-after'),
+    menuBtn = document.querySelector('.header__menu-btn'),
+    headerTopWrapper = document.querySelector('.header__top-wrapper'),
+    headerLogo = document.querySelector('.header__logo-img');
 
 const hideTempWrapper = () => {
     videoTempWrapper.style.display = 'none';
@@ -10,3 +12,13 @@ const hideTempWrapper = () => {
 };
 
 videoWrapper.addEventListener('click', hideTempWrapper);
+
+menuBtn.addEventListener('click', () => {
+    headerTopWrapper.classList.toggle('header__top-wrapper--active');
+    menuBtn.classList.toggle('header__menu-btn--active');
+    if (menuBtn.classList.contains('header__menu-btn--active')) {
+        headerLogo.src = 'img/logo_white.svg';
+    } else {
+        headerLogo.src = 'img/logo.svg';
+    }
+});
