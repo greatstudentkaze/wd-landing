48e9c901710f6a4d4e4d9f004c1b5a12622f741e
gsk <dfyz0r@list.ru>
2020-07-11 17:56:25 +0300

    добавлен poster к видео

--- js/main.js
+++ js/main.js
@@ -3,14 +3,13 @@ const video = document.querySelector('.workstyle__media-video');
 const videoWrapper = document.querySelector('.workstyle__media'),
     videoTempWrapper = document.querySelector('.workstyle__media-after');
 
-
 const hideTempWrapper = () => {
     video.load();
     videoTempWrapper.style.display = 'none';
     video.controls = true;
     video.play();
     videoWrapper.removeEventListener('click', hideTempWrapper);
-}
+};
 
 videoWrapper.addEventListener('click', hideTempWrapper);
 
