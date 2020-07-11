04e3534fee3488221ce186d3d5ca3110e93a1108
gsk <dfyz0r@list.ru>
2020-07-11 18:04:35 +0300

    удалена лишняя перезагрузка видео

--- js/main.js
+++ js/main.js
@@ -4,12 +4,9 @@ const videoWrapper = document.querySelector('.workstyle__media'),
     videoTempWrapper = document.querySelector('.workstyle__media-after');
 
 const hideTempWrapper = () => {
-    video.load();
     videoTempWrapper.style.display = 'none';
     video.controls = true;
-    video.play();
     videoWrapper.removeEventListener('click', hideTempWrapper);
 };
 
 videoWrapper.addEventListener('click', hideTempWrapper);
-
